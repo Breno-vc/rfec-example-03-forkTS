2e10a68027128ffb0448f46d350b5fe28ccc99c8
4lisson777 <alissonlima529@gmail.com>
2025-02-09 20:54:41 -0300

    styles(title): add title component styles

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,13 +1,15 @@
+import styles from './title.module.css'
+
 const H1 = ({ children }) => {
-    return <h1>{children}</h1>
+    return <h1 className={styles.h1}>{children}</h1>
 }
 
 const H2 = ({ children }) => {
-    return <h2>{children}</h2>
+    return <h2 className={styles.h2}>{children}</h2>
 }
 
 const H3 = ({ children }) => {
-    return <h3>{children}</h3>
+    return <h3 className={styles.h3}>{children}</h3>
 }
 
 const Title = {
