15d23bdd7d9a528e5b32b6631c89875c6e27c922
4lisson777 <alissonlima529@gmail.com>
2025-02-09 20:34:00 -0300

    feat(title): add title component

---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,19 @@
+const H1 = ({ children }) => {
+    return <h1>{children}</h1>
+}
+
+const H2 = ({ children }) => {
+    return <h2>{children}</h2>
+}
+
+const H3 = ({ children }) => {
+    return <h3>{children}</h3>
+}
+
+const Title = {
+    H1,
+    H2,
+    H3
+}
+
+export default Title
