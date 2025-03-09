3a48d7464851641e7fd7bc47891ff825ecf01b09
4lisson777 <alissonlima529@gmail.com>
2025-03-09 18:28:42 -0300

    feat(form): WIP

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,36 @@
+import styles from "./Form.module.css"
+
+const Root = (props) => {
+    return <form {...props} className={styles.form}>{props.children}</form>
+}
+
+const Input = (props) => {
+    return <input {...props} className={styles.formControl} type="text" />
+}
+
+const TextArea = (props) => {
+    return <textarea {...props} className={styles.formControl+`${props.disableResize ? ' '+styles.disableResize : ''}`} />
+}
+
+const FormControl = {
+    Input,
+    TextArea
+}
+
+const Form = {
+    Root,
+    FormControl
+}
+
+export default Form
+
+/**
+ * Proxima call vamos trocar esse componente por um de Form que centraliza tudo por la.
+ * Vamos trocar o nome da branch com o rename.
+ * Form.Root
+ * Form.Group
+ * Form.Label
+ * Form.Control.Input
+ * Form.Control.TextArea
+ * Form.ErrorMessage
+ */
